1ca877046b4eb9fe2311c4a177509c791afae784
jucelio <jucelioinfo@gmail.com>
2023-01-31 23:36:38 -0300

    Testes com Bogus e MOQ (Mock)

---
+++ Testes de Software/Features.Tests/04 - Dados Humanos/ClienteBogusTests.cs
@@ -0,0 +1,51 @@
+﻿using Xunit;
+
+namespace Features.Tests
+{
+    [Collection(nameof(ClienteBogusCollection))]
+    public class ClienteBogusTests
+    {
+        private readonly ClienteTestsBogusFixture _clienteTestsFixture;
+
+        public ClienteBogusTests(ClienteTestsBogusFixture clienteTestsFixture)
+        {
+            _clienteTestsFixture = clienteTestsFixture;
+        }
+
+        //        Bogus => Para utilização do Bogus, deve-se instalar ele. O link e comando está abaixo.
+        //https://github.com/bchavez/Bogus        
+        //Nuget Package Bogus
+        //Install-Package Bogus
+
+
+        [Fact(DisplayName = "Novo Cliente Válido")]
+        [Trait("Categoria", "Cliente Bogus Testes")]
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            // Arrange
+            var cliente = _clienteTestsFixture.GerarClienteValido();
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert 
+            Assert.True(result);
+            Assert.Equal(0, cliente.ValidationResult.Errors.Count);
+        }
+
+        [Fact(DisplayName = "Novo Cliente Inválido")]
+        [Trait("Categoria", "Cliente Bogus Testes")]
+        public void Cliente_NovoCliente_DeveEstarInvalido()
+        {
+            // Arrange
+            var cliente = _clienteTestsFixture.GerarClienteInvalido();
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert 
+            Assert.False(result);
+            Assert.NotEqual(0, cliente.ValidationResult.Errors.Count);
+        }
+    }
+}
