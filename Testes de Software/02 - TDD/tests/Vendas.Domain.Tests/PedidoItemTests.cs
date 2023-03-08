a70e959c43497982f82fa668edd7c346dec20ca3
jucelio <jucelioinfo@gmail.com>
2023-03-08 15:03:11 -0300

    TDD - Adicionar Item - Parte 1,2 e 3

---
+++ Testes de Software/02 - TDD/tests/Vendas.Domain.Tests/PedidoItemTests.cs
@@ -0,0 +1,19 @@
+﻿using Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Vendas.Domain.Tests
+{
+    public class PedidoItemTests
+    {
+        [Fact(DisplayName = "Novo Item Pedido com unidades Abaixo do permitido")]
+        [Trait("Categoria", "Item Pedido Tests")]
+        public void AdicionarItemPedido_UnidadesItemAbaixoDoPermitido_DeveRetornarException()
+        {
+            //Arrange & Act & Assert
+            Assert.Throws<DomainException>(() => new PedidoItem(Guid.NewGuid(), "Porduto Teste", Pedido.MIN_UNIDADES_ITEM - 1, 100));
+        }
+    }
+}
