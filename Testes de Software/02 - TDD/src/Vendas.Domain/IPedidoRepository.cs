4daf504fd91eefb119bb728f3bbc39734dd3a408
jucelio <jucelioinfo@gmail.com>
2023-03-27 23:01:47 -0300

    TDD - Pedido - Command Handler - Parte 1

---
+++ Testes de Software/02 - TDD/src/Vendas.Domain/IPedidoRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vendas.Domain
+{
+    public interface IPedidoRepository //: IRepository<Pedido>
+    {
+        //Task<IEnumerable<Pedido>> ObterListaPorClienteId(Guid clienteId);
+        //Task<Pedido> ObterPedidoRascunhoPorClienteId(Guid clienteId);
+        void Adicionar(Pedido pedido);
+        void Atualizar(Pedido pedido);
+
+        //Task<PedidoItem> ObterItemPorPedido(Guid pedidoId, Guid produtoId);
+        void AdicionarItem(PedidoItem pedidoItem);
+        void AtualizarItem(PedidoItem pedidoItem);
+        void RemoverItem(PedidoItem pedidoItem);
+
+        //Task<Voucher> ObterVoucherPorCodigo(string codigo);
+    }
+}
---
+++ Testes de Software/02 - TDD/tests/Vendas.Application.Tests/Pedidos/PedidoCommandHandlerTests.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using Moq;
+using Moq.AutoMock;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Vendas.Application.Commands;
+using Vendas.Domain;
+using Xunit;
+
+namespace Vendas.Application.Tests.Pedidos
+{
+    public class PedidoCommandHandlerTests
+    {
+        //Deve-se instalar MOQ e automock. Comandos: 
+        //install-package MOQ 
+        //install-package MOQ.automock
+
+        [Fact(DisplayName = "Adicionar Item Novo Pedido com Sucesso")]
+        [Trait("Categoria", "Vendas - Pedido Command Handler")]
+        public async Task AdicionarItem_NovoPedido_DeveExecutarComSucesso()
+        {
+            //Arrange
+            var pedidoCommand = new AdicionarItemPedidoCommand(Guid.NewGuid(), Guid.NewGuid(), "Produto Testes", 2, 100);
+
+            var mocker = new AutoMocker();
+            var pedidoHandler = mocker.CreateInstance<PedidoCommandHandler>();
+
+            // Act
+            var result = await pedidoHandler.Handle(pedidoCommand, CancellationToken.None);
+
+            // Assert
+            Assert.True(result);
+            mocker.GetMock<IPedidoRepository>().Verify(r => r.Adicionar(It.IsAny<Pedido>()), Times.Once);
+            mocker.GetMock<IMediator>().Verify(r => r.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Once);
+        }
+    }
+}
